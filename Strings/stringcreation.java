75bffd7ace0a34df92698f6a0174d7b3cf574b1d
Narendra R Giriyappanavar <naren.rg09@gmail.com>
2026-06-21 11:48:56 +0530

    code

---
+++ Strings/stringcreation.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class stringcreation {
+
+    public static void charprint(String fullname) {
+        for(int i=0;i < fullname.length();i++) {
+            System.out.print(fullname.charAt(i) + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) { 
+
+        // declaring an String
+        String str = "Google";
+        System.out.println(str);
+        Scanner sc = new Scanner(System.in);
+
+        // taking string input
+        System.out.println("Enter your firstname: ");
+        String firstname;
+        String lastname;
+        String fullname;
+        firstname = sc.nextLine();
+        System.out.println("Enter your lastname: ");
+        lastname = sc.nextLine();
+        fullname = firstname + " " + lastname;
+
+        // String concatination
+        System.out.println("Hello! " + fullname + " Welcome to Google");
+
+        // finding length using fun
+        System.out.println(fullname.length());
+
+        charprint(fullname);
+    }
+}
